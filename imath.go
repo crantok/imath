f210087909e075579d3e2844a2ac93388d607e87
Justin Hellings <justin.hellings@gmail.com>
2016-09-02 09:31:48 +0100

    Initial comit

---
+++ imath.go
@@ -0,0 +1,47 @@
+// Package imath provides some integer math functions.
+package imath
+
+// Constants stolen from https://groups.google.com/d/msg/golang-nuts/a9PitPAHSSU/ziQw1-QHw3EJ
+const (
+	MaxUint = ^uint(0)
+	MinUint = 0
+	MaxInt  = int(MaxUint >> 1)
+	MinInt  = -MaxInt - 1
+)
+
+// Min gets the minimum value from a set of ints.
+func Min(a ...int) int {
+
+	min := MaxInt
+
+	for _, v := range a {
+		if v < min {
+			min = v
+		}
+	}
+
+	return min
+}
+
+// Max gets the maximum value from a set of ints.
+func Max(a ...int) int {
+
+	max := MinInt
+
+	for _, v := range a {
+		if v > max {
+			max = v
+		}
+	}
+
+	return max
+}
+
+// Abs returns the absolute value of an int.
+func Abs(a int) int {
+	if a < 0 {
+		return -a
+	} else {
+		return a
+	}
+}
